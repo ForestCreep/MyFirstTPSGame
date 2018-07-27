1110b26fbfbb614aa66b442e520e2cd6f6d4795b
ForestCreep <38371710+ForestCreep@users.noreply.github.com>
2018-07-27 17:01:11 +0800

    添加人物模型与动画

---
+++ MyFirstTPS/MyFirstTPS/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Animator _animator;
+    public float kSpeedX = 1.76f;
+    public float kSpeedZ = 2.87f;
+
+    // Use this for initialization
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        _animator.SetFloat("SpeedX", h * kSpeedX);// A D
+        _animator.SetFloat("SpeedZ", v * kSpeedZ);// W S
+    }
+}
