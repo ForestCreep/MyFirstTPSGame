1381238192d341078c6a42bd82466ad6e56cdba7
ForestCreep <38371710+ForestCreep@users.noreply.github.com>
2018-08-04 19:53:51 +0800

    -->解决人物为复活站立时可移动的-->显示玩家对敌人造成的伤害（已完成）Bug（已完成）【去掉勾选的 HasExitTime ,让 trigger 直接变为 false】 -->设置敌人射击最大距离，并向玩家射击（无偏差）并造成伤害（未完成） -->设置当敌人不移动时始终面向玩家（已完成）

--- MyFirstTPS/MyFirstTPS/Assets/Scripts/Enemy/EnemyController.cs
+++ MyFirstTPS/MyFirstTPS/Assets/Scripts/Enemy/EnemyController.cs
@@ -5,18 +5,40 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    public Transform target;
+    public Transform Target;
     private NavMeshAgent _agent;
     private Animator _animator;
     public float Hp = 100;// 生命上限
-    public Transform revivePoint;// 复活点
+    public Transform RevivePoint;// 复活点
     private bool _isAlive = true;
+    public float MaxShootDistance;
+    public GameObject[] Weapons;
+    private GameObject _currentGun;
+    private Weapon _currentWeapon;
+    private float _lastShootTime;
+    private Transform _shootFlash;
+    public float ShootFlashDisappearTime = 0.05f;
 
     // Use this for initialization
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
         _animator = GetComponent<Animator>();
+        // 随机获取一把枪支
+        GetWeaponRandomly();
+    }
+
+    private void GetWeaponRandomly()
+    {
+        var weaponIndex = Random.Range(0, Weapons.Length);
+        foreach (var item in Weapons)
+        {
+            if (item.name == Weapons[weaponIndex].name)
+            {
+                item.SetActive(true);
+                _currentGun = item;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -24,25 +46,71 @@ public class EnemyController : MonoBehaviour
     {
         if (Hp > 0)
         {
-            if (_agent)
+            _agent.SetDestination(Target.position);
+            var velocity = transform.InverseTransformDirection(_agent.desiredVelocity);
+            _animator.SetFloat("SpeedXWithWeapon", velocity.x);
+            _animator.SetFloat("SpeedZWithWeapon", velocity.z);
+
+            // 当达到最大射程范围时可以向玩家射击
+            if (Vector3.Distance(transform.position, Target.position) <= MaxShootDistance)
             {
-                _agent.SetDestination(target.position);
-                var velocity = transform.InverseTransformDirection(_agent.desiredVelocity);
-                _animator.SetFloat("SpeedXWithoutWeapon", velocity.x);
-                _animator.SetFloat("SpeedZWithoutWeapon", velocity.z);
+                ShootToPlayer();
+                transform.LookAt(Target);
             }
         }
     }
 
+    private void ShootToPlayer()
+    {
+        var enemyPosition = transform.position;
+        var targetPosition = Target.position;
+        var direction = Target.position - transform.position;
+
+        // 向玩家发射射线
+        Ray ray = new Ray(new Vector3(enemyPosition.x, enemyPosition.y + 1.4f, enemyPosition.z), new Vector3(direction.x, direction.y, direction.z));
+        Debug.DrawRay(ray.origin, ray.direction * 20);
+
+        var shootSoundSource = _currentGun.GetComponent<AudioSource>();
+        if (shootSoundSource)
+        {
+            _currentWeapon = _currentGun.GetComponent<Weapon>();
+            if (_currentWeapon && Time.time - _lastShootTime >= 3)
+            {
+                //Debug.Log(Time.time);
+                if (_animator)
+                {
+                    shootSoundSource.Play();
+                    _lastShootTime = Time.time;
+                    //_animator.SetTrigger("IsShoot");
+                    _shootFlash = _currentGun.transform.Find("MuzzleFlash");
+                    if (_shootFlash)
+                    {
+                        _shootFlash.gameObject.SetActive(true);
+                        StartCoroutine("HideShootFlash");
+                    }
+
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        Debug.Log(hit.collider.name);
+                        // 贴合弹孔
+
+                        // 击中玩家使其掉血
+                        hit.collider.SendMessage("ReceiveDamage", _currentWeapon.DamageValue, SendMessageOptions.DontRequireReceiver);
+                    }
+                }
+            }
+        }
+    }
+
+    private IEnumerator HideShootFlash()
+    {
+        yield return new WaitForSeconds(ShootFlashDisappearTime);
+        _shootFlash.gameObject.SetActive(false);
+    }
+
     private void ReceiveDamage(float value)
     {
-        // hp=10;
-        // value=30
-        // hp=-20
-        // -->120
-        // value=10
-        // -->130
-        // -->100
         if (Hp > 0)
         {
             Hp -= value;
@@ -59,14 +127,16 @@ public class EnemyController : MonoBehaviour
                 CountManager.Instance.OnDamagePlayerToEnemy(Hp);
             }
         }
+
+        CountManager.Instance.SetEnemyHp(Hp);
     }
 
     private void OnFinishedDead()
     {
         _animator.SetTrigger("Revive");
-        _agent.isStopped = false;
         _isAlive = true;
         Hp = 100;
-        transform.position = revivePoint.transform.position;
+        transform.position = RevivePoint.transform.position;
+        _agent.isStopped = false;
     }
 }
---
+++ MyFirstTPS/MyFirstTPS/Assets/Scripts/UI/SumPlayerToEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SumPlayerToEnemy : MonoBehaviour
+{
+    private Text _playerToEnemy;
+
+    // Use this for initialization
+    void Start()
+    {
+        _playerToEnemy = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _playerToEnemy.text = "玩家对敌人造成的总伤害：" + CountManager.SumDamagePlayerToEnemy;
+    }
+}
