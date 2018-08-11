3185eb150e5464ae0c41700382b1ee62bccd10b2
ForestCreep <38371710+ForestCreep@users.noreply.github.com>
2018-08-11 10:02:14 +0800

    未加小球返回特效的最终版本

---
+++ PhysicBalls/Assets/Scripts/Controllers/BallUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallUp : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        var rg = collision.GetComponent<Rigidbody2D>();
+        rg.AddForce(new Vector2(collision.transform.position.x, collision.transform.position.y + 35), ForceMode2D.Impulse);
+    }
+}
